fc506e4799ff166439972e517515ce8cd006441b
prasanth <prasanthpayne@gmail.com>
2016-10-07 17:27:50 +0530

    app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y,speed) {
+    this.x=x;
+    this.y=y;
+    this.speed=speed;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -14,24 +17,41 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
 };
 
 // Now write your own player class
-var Player = function(){
-    this.sprite="char-boy.png";
+var Player = function(x,y,speed){
+    this.x=x;
+    this.y=y;
+    this.speed=speed;
+    this.sprite="images/char-boy.png";
 }
+Player.prototype.update=function(dt){
+
+};
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+
 // This class requires an update(), render() and
 // a handleInput() method.
+
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+ 
+
 
 
 
