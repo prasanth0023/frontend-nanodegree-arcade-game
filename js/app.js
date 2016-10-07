4efb1df5c735924ec5de65491412db1f3749cd4f
prasanth <prasanthpayne@gmail.com>
2016-10-07 17:50:22 +0530

    app.js

--- js/app.js
+++ js/app.js
@@ -50,7 +50,8 @@ Player.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
- 
+ var player =new Player(200,383,50);
+ var allEnemies=[];
 
 
 
